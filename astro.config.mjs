3f9242361d00c5c351d1406f0e345c2874fd36ce
Cristian Cassetta <cristiancassetta@gmail.com>
2024-06-08 20:39:35 +0200

    Updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
       prefixDefaultLocale: true,
     },
   },
-  site: "https://astro-micro.vercel.app",
+  site: "https://the-cassettas-blog.vercel.app",
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
     shikiConfig: {
